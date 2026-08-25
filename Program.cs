48b16fee3034829cdb86a26d6e41bd5fe2666552
mambo123-gg <pantat.tesn@bumail.net>
2026-08-25 19:19:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            // Lab02 Part A
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level { level}");
+            Console.WriteLine($"HP {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier:{critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+        }
+    }
+}
