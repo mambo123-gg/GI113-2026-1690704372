5a38d5539947d3cdf75cd7ee6bb61ccc248d84ba
mambo123-gg <pantat.tesn@bumail.net>
2026-08-25 16:31:16 +0700

    add lap2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bossName = "Kirin";
+            char rank = 's';
+            int Level = 7;
+            int maxHp = 240;
+            int currenHp = 175;
+            float attackPower = 50.40f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            
+            Console.WriteLine(bossName);
+            Console.WriteLine($"Name: {bossName} {Level}");
+            Console.WriteLine($"HP: {currenHp * 100/ maxHp}%");
+        }
+    }
+}
