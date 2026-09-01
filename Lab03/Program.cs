108bd9d76061ee033da73b4e86b6e91b4d26a6cd
Hussaluk <pantat.tesn@bumail.net>
2026-09-01 16:18:19 +0700

    update add script to lab03

--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level} / {MaxLevel}" +
+            $"\nCurrent HP: {currentHp} / {maxHp}" +
+            $"\nAttack Power: {attackPower}" +
+            $"\nCritical Multiplier: {critMultiplier}" +
+            $"\nIs Boss: {isBoss}");
         }
     }
 }
