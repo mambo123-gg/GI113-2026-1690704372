b71a86baea534d86828d8adc3f94ca08aedbe422
Hussaluk <hussaluk.srip@bumail.net>
2026-09-08 15:18:10 +0700

    Add new adventure and difficult select

---
+++ Lab04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("| NEW ADVENTURE |");
+            Console.WriteLine("+-------------------+");
+
+            Console.Write("Name your hero: ");
+            string heroName = Console.ReadLine();
+
+            Console.WriteLine($"\n\"Welcome, {heroName}. Your journey begins.....\"\n");
+
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("|  DIFFICULTY SELECT |");
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("Choose difficulty (1-3): ");
+            
+            int difficulty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\n\" Difficulty {difficulty} selected. Good luck out there...\"\n");
+        }
+    }
+}
