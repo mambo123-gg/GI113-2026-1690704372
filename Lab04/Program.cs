dfd3d8ab89e9682c36ce1291c12d9d3a4ebaed13
Hussaluk <hussaluk.srip@bumail.net>
2026-09-08 15:25:23 +0700

    add itemshop line

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -20,6 +20,14 @@
             
             int difficulty = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"\n\" Difficulty {difficulty} selected. Good luck out there...\"\n");
+
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("|  ITEM SHOP |");
+            Console.WriteLine("+-------------------+");
+            Console.WriteLine("How many potion?");
+            bool isValid = int.TryParse(Console.ReadLine(), out int quantity);
+            Console.WriteLine($"Valid input: {isValid}");
+            Console.WriteLine($"Quantity: {quantity}");
         }
     }
 }
