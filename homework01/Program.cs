eed6ae7b846ef06f56f1ebcb2e83f3bafa6057c4
mambo123-gg <pantat.tesn@bumail.net>
2026-09-14 21:36:20 +0700

    Add name and student id

--- homework01/Program.cs
+++ homework01/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace homework01
+﻿/*
+ * Student ID :1690704372
+ * Name       :pantat tesngamtuan
+ * Section    :129D
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace homework01
 {
     internal class Program
     {
