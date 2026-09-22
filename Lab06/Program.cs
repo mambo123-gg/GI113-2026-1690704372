e46e9dc1ba142d4fc4418c8fa33030b35332940c
mambo <pantat.tesn@bumail.net>
2026-09-22 15:50:25 +0700

    Add lab06

---
+++ Lab06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = 7;
+            if (level >= 5)
+            {
+                Console.WriteLine("The door opens.");
+
+            else if (level >= 10 )
+            {
+                Console.WriteLine("Boss floor unlocked.");
+            }
+            else
+            {
+                Console.WriteLine("The door stays shut.");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
