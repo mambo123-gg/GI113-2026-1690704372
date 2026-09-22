d3dabe2d455086ff26fb0d13f6cc0eb2978b8b4d
mambo <pantat.tesn@bumail.net>
2026-09-22 14:46:47 +0700

    Create Program.cs

---
+++ Lab05/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Game Title, Sub-title
+            Console.WriteLine("==>> MY VIDEO GAME vERY <<==");
+            Console.WriteLine("Hero Hp: ");
+            bool heroHpok = int.TryParse(Console.ReadLine(), out int heroHp);
+            Console.WriteLine("Hero ATK: ");
+            bool heroAtkok = int.TryParse(Console.ReadLine(), out int heroAtk);
+            Console.WriteLine("Hero DFS: ");
+            bool heroDfsok = int.TryParse(Console.ReadLine(), out int heroDfs);
+            Console.WriteLine($"Hero Stats: HP: {heroHp}, ATK: {heroAtk}, DFS: {heroDfs}");
+        }
+    }
+}
