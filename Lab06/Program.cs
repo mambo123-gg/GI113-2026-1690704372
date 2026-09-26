24a6486653c04a2c06bb4b7e6ea5caf2ad189bec
mambo123-gg <pantat.tesn@bumail.net>
2026-09-26 15:57:46 +0700

    add lab06 script

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -1,23 +1,59 @@
-﻿namespace Lab06
-{
-    internal class Program
+﻿
+        
+        
+            namespace Lab06
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            
+            static void Main(string[] args)
+            {
+
 
                 int normalattack = 10;
                 int criticalattack = 20;
                 int potionhpheal = 30;
                 int monsterhp = 100;
                 int herohp = 100;
-                
+
                 Console.WriteLine("====Game title ethernie====");
 
                 Console.WriteLine("Action1 normalattack.");
                 Console.WriteLine("Action2 criticalattack.");
                 Console.WriteLine("Action3 Drink potionhpheal.");
 
+                Console.WriteLine("+++>CHOOSE YOUR NEXT MOVE (1-3)");
+                bool inputValid = int.TryParse(Console.ReadLine() out int choice);
+                if (!inputValid || choice < 1 || choice > 3)
+                {
+                    Console.WriteLine("Invalid input, please choose between 1-3 only!");
+                }
+                else if (choice == 1)
+                {
+                    monsterhp -= normalattack;
+                    if (monsterhp <= 0)
+                    {
+                        Console.WriteLine($"monster got hit my sword normol attack, took {normalattack}DMG. Monster died!.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"monster got hit my sword normol attack, took {normalattack}DMG. Monster has {monsterhp} HP left.");
+                    }
+                }
+                else if (choice == 2)
+                {
+                    monsterhp -= criticalattack;
+                }
+                else
+                {
+                    herohp += potionhpheal;
+                }
+
+
+
+                }
             }
+        }
+)
+        }
     }
 }
